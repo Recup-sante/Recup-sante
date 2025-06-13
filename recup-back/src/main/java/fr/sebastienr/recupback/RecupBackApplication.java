a69ef46a8a0320f10b495b122f00204e44a66a7e
sebastien85540 <sebastien.reynaud123@orange.fr>
2025-06-13 17:16:01 +0200

    add backend application

---
+++ recup-back/src/main/java/fr/sebastienr/recupback/RecupBackApplication.java
@@ -0,0 +1,13 @@
+package fr.sebastienr.recupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecupBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecupBackApplication.class, args);
+    }
+
+}
---
+++ recup-back/src/test/java/fr/sebastienr/recupback/RecupBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sebastienr.recupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecupBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
